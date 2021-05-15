63a7391501691efd197aca4d8a75bb35e836fde5
ppshanti <prashantisfdc@gmail.com>
2021-05-15 08:43:49 -0700

    Added comments and reduced width.

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 console.log("Project start !");
 
 //variables
-
 let timerEle = document.getElementById("timer");
-
 console.log(timerEle);
 
 let seconds = 180;
-
-let tower1 = ["1", "2", "3"];
-
+let tower1 = ["1", "2", "3"]; // array to check win condition
 let tower2 = [];
-
 let tower3 = [];
 
-//start game
 
+
+//start game timer
 function startGame() {
   // start timer on scroll to game
-
-  setInterval(function () {
+ let myTime= setInterval(function () {
     seconds--;
-
     timerEle.innerText = seconds;
-
     if (seconds === 0) {
       alert("Times up !!");
 
@@ -31,24 +24,27 @@ function startGame() {
 
       seconds = 180;
     }
+    if (tower3.length === 3) {
+      alert("you won the game");
+      clearInterval(myTime);
+      }
   }, 1000);
+
 }
 
 //drag and drop to different tower
-
 function allowDrop(ev) {
-  ev.preventDefault();
+  ev.preventDefault(); //stops refresh
 }
-
+//drag
 function drag(ev) {
+  //set disk id
   ev.dataTransfer.setData("text", ev.target.id);
-
   console.log(ev.dataTransfer.setData("text", ev.target.id));
 }
-
+//drop
 function drop(ev) {
   ev.preventDefault();
-
   if (
     !(
       ev.srcElement.id === "tower1" ||
@@ -58,46 +54,42 @@ function drop(ev) {
   ) {
     return;
   }
-
   console.log(ev.srcElement);
-
+  //get the disk id
   var data = ev.dataTransfer.getData("text");
-
+//if disk in tower is smaller than one dragging --srcElement -td element
   if (ev.srcElement.childNodes && ev.srcElement.childNodes.length > 0) {
+    //do not allow small disk to avoid more than 1 element drop
     if (ev.srcElement.childNodes[0].id < data) {
       return;
     }
   }
-
+//prevent drag of middle disk
   if(!checkAllPreviOuvsValues(ev.srcElement.id )){
     return;
   }
-
+//check if smaller disk in tower dont drop
   if (!checkIfIsvalidMove(data)) {
     return;
   }
-
+  //remove and add array (not html element)
   removeAndAdd(data, ev.srcElement.id);
-
   ev.target.appendChild(document.getElementById(data));
-
-  if (tower3.length === 3) {
-    alert("you won the game");
-  }
-
-  console.log(data);
+     //check win if has 3 disk
+    //  if (tower3.length === 3) {
+    //   alert("you won the game");
+    //   }
 }
-
+//removes specific element from array
 function removeItemOnce(arr, value) {
   var index = arr.indexOf(value);
-
   if (index > -1) {
+    //remove specific element
     arr.splice(index, 1);
   }
-
   return arr;
 }
-
+// remove current tower and add to target tower
 function removeAndAdd(tragetId, srcId) {
   if (document.getElementById(tragetId).parentNode.id == "tower3") {
     removeItemOnce(tower3, tragetId);
@@ -105,24 +97,20 @@ function removeAndAdd(tragetId, srcId) {
   if (document.getElementById(tragetId).parentNode.id == "tower1") {
     removeItemOnce(tower1, tragetId);
   }
-
   if (document.getElementById(tragetId).parentNode.id == "tower2") {
     removeItemOnce(tower2, tragetId);
   }
-
   if (srcId == "tower3") {
     tower3.push(tragetId);
   }
-
   if (srcId == "tower2") {
     tower2.push(tragetId);
   }
-
   if (srcId == "tower1") {
     tower1.push(tragetId);
   }
 }
-
+//checks if disk can be placed in the tower
 function checkIfIsvalidMove(tragetId) {
   if (document.getElementById(tragetId).parentNode.id == "tower3") {
     return minVlaueCheck(tower3, tragetId);
@@ -135,37 +123,36 @@ function checkIfIsvalidMove(tragetId) {
     return minVlaueCheck(tower2, tragetId);
   }
 }
-
+//checks if tower has small or large disks
 function checkAllPreviOuvsValues(srcId){
   if (srcId == "tower3") {
     return checkMaxValue(tower3, srcId);
   }
 
   if (srcId == "tower2") {
-    return checkMaxValue(tower3, srcId);
+    return checkMaxValue(tower2, srcId);
   }
 
   if (srcId == "tower1") {
-    return checkMaxValue(tower3, srcId);
+    return checkMaxValue(tower1, srcId);
   }
 }
-
+//
 function checkMaxValue(tower, srcId){
   var retVal = true;
-
   tower.forEach((element) => {
     if (parseInt(element) > parseInt(srcId)) {
       retVal = false;
     }
   });
-
   return retVal;
 }
 
+//drop in tower only if disk is the smallest
 function minVlaueCheck(tower, targetId) {
   var retVal = true;
-
   tower.forEach((element) => {
+    //get id and convert to int to compare
     if (parseInt(element) < parseInt(targetId)) {
       retVal = false;
     }
